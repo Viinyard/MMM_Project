b471133dd73fd9d822e1bc22180842e75e93f86f
AdrienLeblanc <adrienleblanc53@gmail.com>
2019-02-14 19:25:20 +0100

    transfert pc fixe

---
+++ app/src/main/java/fr/istic/mmm/my_project/Choix.java
@@ -0,0 +1,20 @@
+package fr.istic.mmm.my_project;
+
+public class Choix {
+
+    public String valeur;
+
+    public Choix(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public void setValeur(String valeur) {
+        this.valeur = valeur;
+    }
+
+
+    public String getValeur() {
+        return valeur;
+    }
+
+}
