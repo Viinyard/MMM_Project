1872eaf5132c52ca9c2f42b059201a91b9ea3d05
vinyard <antoineleval@gmail.com>
2019-02-15 14:58:02 +0100

    add remove action

--- app/src/main/java/fr/istic/mmm/my_project/AdminCourseActivity.java
+++ app/src/main/java/fr/istic/mmm/my_project/AdminCourseActivity.java
@@ -5,13 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminCourseActivity extends AppCompatActivity {
 
-    private Button btnCreatePoll, btnViewPoll, btnLogout;
+    private Button btnCreatePoll, btnViewPoll, btnLogout, btnRemove;
 
     private FirebaseAuth mAuth;
 
@@ -24,13 +27,16 @@ public class AdminCourseActivity extends AppCompatActivity {
         final Course course = (Course) intent.getSerializableExtra("course");
 
         btnCreatePoll = (Button) findViewById(R.id.button_create_poll);
-
+        btnRemove = (Button) findViewById(R.id.remove);
 
 
         btnViewPoll = (Button) findViewById(R.id.view_poll);
         btnLogout = (Button) findViewById(R.id.logout);
 
         mAuth = FirebaseAuth.getInstance();
+
+
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
@@ -41,6 +47,11 @@ public class AdminCourseActivity extends AppCompatActivity {
         if(!course.getOwner().equals(currentUser.getEmail())) {
             btnCreatePoll.setEnabled(false);
             btnCreatePoll.setVisibility(View.INVISIBLE);
+        }
+
+        if(!currentUser.getEmail().equals(course.getOwner())) {
+            btnRemove.setEnabled(false);
+            btnRemove.setVisibility(View.INVISIBLE);
         }
 
         btnCreatePoll.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +80,14 @@ public class AdminCourseActivity extends AppCompatActivity {
             }
         });
 
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.child("course").child(course.name).removeValue();
+                Toast.makeText(AdminCourseActivity.this, "Deleted successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
 
 
     }
--- app/src/main/java/fr/istic/mmm/my_project/AdminPollActivity.java
+++ app/src/main/java/fr/istic/mmm/my_project/AdminPollActivity.java
@@ -6,13 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminPollActivity extends AppCompatActivity {
 
-    private Button btnAnswering, btnViewResulsts, btnLogout;
+    private Button btnAnswering, btnViewResulsts, btnLogout, btnRemove;
 
     private FirebaseAuth mAuth;
 
@@ -24,10 +27,13 @@ public class AdminPollActivity extends AppCompatActivity {
         btnAnswering = (Button) findViewById(R.id.repondre_action);
         btnViewResulsts = (Button) findViewById(R.id.view_results_action);
         btnLogout = (Button) findViewById(R.id.logout);
+        btnRemove = (Button) findViewById(R.id.remove);
 
         mAuth = FirebaseAuth.getInstance();
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
+
 
         Intent intent = getIntent();
         final Course course = (Course) intent.getSerializableExtra("course");
@@ -35,6 +41,11 @@ public class AdminPollActivity extends AppCompatActivity {
 
         if(currentUser == null) {
             startActivity(new Intent(AdminPollActivity.this, MainActivity.class));
+        }
+
+        if(!currentUser.getEmail().equals(course.getOwner())) {
+            btnRemove.setEnabled(false);
+            btnRemove.setVisibility(View.INVISIBLE);
         }
 
         btnAnswering.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +65,17 @@ public class AdminPollActivity extends AppCompatActivity {
                 intent.putExtra("course", course);
                 intent.putExtra("sondage", sondage);
                 startActivity(intent);
+            }
+        });
+
+        final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.child("sondage").child(course.name).child(sondage.question).removeValue();
+                Toast.makeText(AdminPollActivity.this, "Deleted successfully", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
 
